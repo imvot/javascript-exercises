4f9ef3aff77ac165a3ebca409d3c243ef3ab0622
imvot <110433315+imvot@users.noreply.github.com>
2023-04-09 15:13:10 -0400

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove /* toRemove only also works */) {
+    // Not required with the spread syntax already in the arguments
+    //toRemove = [...arguments];
+    //toRemove.shift();
+    return array.filter( value => !( toRemove.includes(value) ) );
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
