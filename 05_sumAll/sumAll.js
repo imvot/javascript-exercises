a37f4292bf55667c621c070fe64cf7b92e771c99
imvot <110433315+imvot@users.noreply.github.com>
2023-04-09 16:32:13 -0400

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    if(typeof(start) != "number" || typeof(stop) != "number") return "ERROR";
+    if(start < 0 || stop < 0) return "ERROR";
 
+    let sum = 0;
+    if(start <= stop) {
+        for(i=start; i<=stop; i++) {
+            sum += i;
+        }
+    } else {
+        for(i=start; i>=stop; i--) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
