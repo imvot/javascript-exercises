c27da51001ef877ddc42dc46b7a12fca7859e3d7
imvot <110433315+imvot@users.noreply.github.com>
2023-04-09 17:01:19 -0400

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+    return Math.round(
+        ( 
+            (farenheit-32)*(5/9)
+        ) * 10 
+    ) / 10;
+    //one line statement with one parenthesis level removed would have been better
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+    return Math.round(
+        (
+            (celcius*(9/5))+32 
+        ) * 10 
+    ) / 10;
+    //one line statement with one parenthesis level removed would have been better
 };
+
 
 // Do not edit below this line
 module.exports = {
