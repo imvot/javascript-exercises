a84b7d79616d1f0408d9e320515e135dbfdbf35a
imvot <110433315+imvot@users.noreply.github.com>
2023-04-09 16:45:12 -0400

    Complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 4 == 0) {
+        if(year % 100 == 0) {
+            if(year % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
+    // shorter version
+    // return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+    // or
+    // if(year % 4 == 0) {
+        // if(year % 100 != 0 || year % 400 == 0) {
+            // return true;
+        // }
+    // }
+    // return false
 };
 
 // Do not edit below this line
