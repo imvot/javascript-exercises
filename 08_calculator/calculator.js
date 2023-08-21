3ba1ba2a9f652079e2d8b81fd86412199bed2412
imvot <110433315+imvot@users.noreply.github.com>
2023-08-21 12:20:17 -0700

    Check and apply solution to my calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,18 +1,24 @@
 const add = function(...numbers) {	
+    // Solution's simpler
+    // Mine works with more than two numbers
     return numbers.reduce((total, nb) => total + nb, 0)
 };
 
 const subtract = function(...numbers) {
+    // Same here
     return numbers
         .slice(1)
         .reduce((total, nb) => total - nb, numbers[0])
 };
 
 const sum = function(numbers) {
+    // Same as in the solution
     return numbers.reduce((total, nb) => total + nb, 0)
 };
 
 const multiply = function(numbers) {
+    // Does work if array is empty
+    // add array.length ? ... : 0
     return numbers.reduce((total, nb) => total * nb, 1)
 };
 
@@ -21,6 +27,7 @@ const power = function(nb, pw) {
 };
 
 const factorial = function(nb) {
+    // Same as in the solution
     if(nb == 0) return 1
     return nb * factorial(nb-1)
 };
