70b06e2b2f360bba42cf9eb2f2372845d2dedce9
imvot <110433315+imvot@users.noreply.github.com>
2023-08-22 09:47:33 -0700

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const nonLetters = /[\.,?!\ ]/g  
+    return text
+        .replace(nonLetters, "")
+        .toLowerCase()
+        .split("")
+        .every((letter, index, letters) => letter == letters[letters.length - index - 1]);
 };
 
 // Do not edit below this line
