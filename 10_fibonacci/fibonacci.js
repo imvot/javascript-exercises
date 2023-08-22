eed27c4ee418e5379b08e907376a96ec750b0a6d
imvot <110433315+imvot@users.noreply.github.com>
2023-08-22 10:39:38 -0700

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(position) {
+    position = +position 
+    if(position < 0) return "OOPS";
+    let [a, b] = [1, 1] // Starting numbers
+    let sum; 
+    for(let i=1; i<position; i++) {
+        sum = a + b
+        a = b;
+        b = sum;
+    };
+    return a;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
