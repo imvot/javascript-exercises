e53717a28781d646b22898efa66908b6e4f932c4
imvot <110433315+imvot@users.noreply.github.com>
2023-08-22 09:51:31 -0700

    Compare solution for my palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
 const palindromes = function (text) {
     const nonLetters = /[\.,?!\ ]/g  
+    // Removing of nonLetters is good, but comparing every letter to its opposed one is overkilled
+    // Reversing the string and comparing is way more straight forward
     return text
         .replace(nonLetters, "")
         .toLowerCase()
