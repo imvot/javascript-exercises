d345854b26c39273ea72eada97c8b5abb4b95538
imvot <110433315+imvot@users.noreply.github.com>
2023-08-22 11:12:06 -0700

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.reduce((oldestSoFar, person) => {
+        const oldestYearofDeath = "yearOfDeath" in oldestSoFar ? oldestSoFar.yearOfDeath : currentYear
+        const personYearofDeath = "yearOfDeath" in person ? person.yearOfDeath : currentYear
+        return (oldestYearofDeath - oldestSoFar.yearOfBirth) > (personYearofDeath - person.yearOfBirth) ? oldestSoFar : person
+    }, {yearOfBirth: currentYear})
 };
 
 // Do not edit below this line
