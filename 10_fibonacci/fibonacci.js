44e4e38fc45b62327cd480292a2a25da7c029c85
imvot <110433315+imvot@users.noreply.github.com>
2023-08-22 10:46:54 -0700

    Compare solution to my fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,13 @@
 const fibonacci = function(position) {
-    position = +position 
+    position = +position // Required ?
     if(position < 0) return "OOPS";
+    // Didn't specify 0 for position 0
+    // Mine has flexible starting numbers
+    // Solution kinda cheated by starting a position before(a = 0 instead of 1)
     let [a, b] = [1, 1] // Starting numbers
     let sum; 
     for(let i=1; i<position; i++) {
+        // Same as in solution but with different names/method to reassign b value
         sum = a + b
         a = b;
         b = sum;
